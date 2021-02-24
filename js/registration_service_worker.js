657cccc5cfb6d1aa91b6640fe2d7939b8d95b748
PashaOsadchi <38135316+PashaOsadchi@users.noreply.github.com>
2021-02-24 06:39:33 +0200

    Update and rename service_worker.js to registration_service_worker.js

--- js/registration_service_worker.js
+++ js/registration_service_worker.js
@@ -2,10 +2,10 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
       navigator.serviceWorker.register('/sw.js', { scope: "/"}).then(function(registration) {
         // Registration was successful
-        // console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        // console.log('Реєстрація Service Worker пройшла успішно: ', registration.scope);
       }, function(err) {
         // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
+        console.log('Виникла помилка при реєстрації Service Worker: ', err);
       });
     });
   }
