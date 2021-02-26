c97d2d58a94d76e9888fc4e23de311591422f83e
PashaOsadchi <38135316+PashaOsadchi@users.noreply.github.com>
2021-02-26 11:33:31 +0200

    Delete js directory

--- js/map_listener.js
+++
@@ -1,7 +0,0 @@
-// Визначає чи змінювалась карта після певної події false - не змінювалась
-let map_bounds_changed = false;
-
-// Прослуховувач події який спрацьовує коли змінився центр карти або маштаб карти
-const listener_bounds_changed = google.maps.event.addListenerOnce(map, "bounds_changed", function () {
-    map_bounds_changed = true;
-});
