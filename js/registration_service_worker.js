61fca715fec2142b7ba3b991f66b150c0845d118
PashaOsadchi <pashka.osadchi@gmail.com>
2021-03-17 10:16:14 +0200

    Оновлення

--- js/registration_service_worker.js
+++ js/registration_service_worker.js
@@ -15,12 +15,3 @@ if ("serviceWorker" in navigator) {
         );
     });
 }
-
-/* if ("serviceWorker" in navigator) {
-    self.addEventListener("load", async () => {
-        const container = navigator.serviceWorker;
-        if (container.controller === null) {
-            const reg = await container.register("/sw.js");
-        }
-    });
-} */
