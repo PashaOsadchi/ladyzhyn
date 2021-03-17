82dd5e30ca5a3ef5afe33a3524b63c97830a8776
PashaOsadchi <pashka.osadchi@gmail.com>
2021-03-17 10:13:27 +0200

    Update registration_service_worker.js

--- js/registration_service_worker.js
+++ js/registration_service_worker.js
@@ -1,24 +1,26 @@
 // https://habr.com/ru/post/547436/
 // https://developers.google.com/web/tools/workbox/
 
-/* if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js', { scope: "/"}).then(function(registration) {
-        // Registration was successful
-        // console.log('Реєстрація Service Worker пройшла успішно: ', registration.scope);
-      }, function(err) {
-        // registration failed :(
-        console.log('Виникла помилка при реєстрації Service Worker: ', err);
-      });
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js", { scope: "/" }).then(
+            function (registration) {
+                // Registration was successful
+                // console.log('Реєстрація Service Worker пройшла успішно: ', registration.scope);
+            },
+            function (err) {
+                // registration failed :(
+                console.log("Виникла помилка при реєстрації Service Worker: ", err);
+            }
+        );
     });
-  } */
+}
 
-
-  if ("serviceWorker" in navigator) {
+/* if ("serviceWorker" in navigator) {
     self.addEventListener("load", async () => {
         const container = navigator.serviceWorker;
         if (container.controller === null) {
             const reg = await container.register("/sw.js");
         }
     });
-}
+} */
