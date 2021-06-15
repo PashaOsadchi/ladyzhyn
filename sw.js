76136f5b8a474b1714312e859d9bc0f7b42a2259
PashaOsadchi <pashka.osadchi@gmail.com>
2021-06-15 11:49:15 +0300

    Оновлено сервіс воркер

--- sw.js
+++ sw.js
@@ -1,41 +1,4 @@
 self.addEventListener("fetch", function (event) {
     // тут нічого не робити, просто реєструвати всі запити мережі
     // console.log(event.request.url);
-
-    return event.respondWith(
-        // Перевірте, чи дані в кеші
-        caches.match(event.request).then(function (response) {
-            if (response) {
-                return response;
-            }
-            // Якщо у нас немає ресурсу в кеші, зробіть запит і кешуйте його
-            return fetch(event.request).then(function (response) {
-                caches.open(cacheStatic).then(function (cache) {
-                    cache.add(event.request.url);
-                });
-
-                return response;
-            });
-        })
-    );
-
-    // Кешувати статичний ресурс, але не зображення
-    /* if (event.request.url.indexOf('https://ladyzhyn.com/') !== -1 && event.request.url.search(/\.(svg|png|jpeg|jpg|gif|webp)/) === -1) {
-        return event.respondWith(
-            // Перевірте, чи дані в кеші
-            caches.match(event.request).then(function (response) {
-                if (response) {
-                    return response;
-                }
-                // Якщо у нас немає ресурсу в кеші, зробіть запит і кешуйте його
-                return fetch(event.request).then(function (response) {
-                    caches.open(cacheStatic).then(function (cache) {
-                        cache.add(event.request.url);
-                    });
-
-                    return response;
-                });
-            })
-        );
-    } */
 });
