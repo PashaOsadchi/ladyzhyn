61670682a7359e45fd22cc2374a7ef176effb94a
PashaOsadchi <pashka.osadchi@gmail.com>
2021-06-15 14:45:20 +0300

    Оновлено сервіс воркер

--- sw.js
+++ sw.js
@@ -25,21 +25,7 @@ addEventListener("fetch", function (event) {
     }
 }); */
 
-self.addEventListener("install", (event) => event.waitUntil(caches.open("v1").then((cache) => cache.addAll(["/"]))));
-
-/* addEventListener("fetch", function (event) {
-    if (event.request.headers.get("Accept").includes("text/html")) {
-        event.respondWith(
-            fetch(event.request)
-                .then(function (response) {
-                    return response;
-                })
-                .catch(function (error) {
-                    return caches.match("index.html");
-                })
-        );
-    }
-}); */
+self.addEventListener("install", (event) => event.waitUntil(caches.open("v1").then((cache) => cache.addAll(["/index.html"]))));
 
 self.addEventListener("fetch", (event) => {
     event.respondWith(
