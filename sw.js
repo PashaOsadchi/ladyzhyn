2f09e1ab5dc919b37fd85290e8fe8dd0510ec4bc
PashaOsadchi <pashka.osadchi@gmail.com>
2021-06-15 15:06:14 +0300

    Оновлено сервіс воркер

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-/* self.addEventListener("fetch", function (event) {
-    // тут нічого не робити, просто реєструвати всі запити мережі
-    // console.log(event.request.url);
-}); */
-
 // Кешує всі ресурси тому викликає помилку переповнення кешу
 // self.addEventListener("install", (event) => event.waitUntil(caches.open("v1").then((cache) => cache.addAll(["/"]))));
 
 // Кешує тільки визначені ресурси
 self.addEventListener("install", (event) => event.waitUntil(caches.open("v1").then((cache) => cache.addAll(["/index.html"]))));
+
+self.addEventListener("fetch", function (event) {
+    // тут нічого не робити, просто реєструвати всі запити мережі
+    // console.log(event.request.url);
+});
 
 /* addEventListener("fetch", function (event) {
     if (event.request.headers.get("Accept").includes("text/html")) {
@@ -23,7 +23,8 @@ self.addEventListener("install", (event) => event.waitUntil(caches.open("v1").th
     }
 }); */
 
-self.addEventListener("fetch", (event) => {
+// Викликає помилку
+/* self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches
             .match(event.request)
@@ -42,4 +43,4 @@ self.addEventListener("fetch", (event) => {
             })
             .catch(() => console.log("not found"))
     );
-});
+}); */
